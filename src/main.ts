292d1a654247e6d3109306fbcc953e8ac845f165
nano-ai-labs <cruzfloresjj@gmail.com>
2026-02-10 16:44:04 -0600

    se sube docker

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-console.log("ENV GOOGLE_CLIENT_ID:", process.env.GOOGLE_CLIENT_ID);
+  
+  console.log("ENV GOOGLE_CLIENT_ID:", process.env.GOOGLE_CLIENT_ID);
 
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({
@@ -13,8 +14,14 @@ console.log("ENV GOOGLE_CLIENT_ID:", process.env.GOOGLE_CLIENT_ID);
     forbidNonWhitelisted: true,
   }));
 
+  // 1. Cambiamos PORT_NUMBER por PORT (Google inyecta 'PORT' automáticamente)
   const port = process.env.PORT_NUMBER || 4000;
-  await app.listen(port);
-  console.log(`API: http://localhost:${port}/api`);
+
+  // 2. IMPORTANTE: Agregamos '0.0.0.0' para que sea visible fuera del contenedor
+  // Esto no afecta tu trabajo local, pero es VITAL para el despliegue.
+  await app.listen(port, '0.0.0.0');
+
+  // Ajustamos el log para que muestre la IP correcta
+  console.log(`API corriendo en el puerto: ${port}`);
 }
 bootstrap();
